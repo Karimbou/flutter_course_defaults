df010cfe054d99b761f6bb2f8b3001eb7620366c
Karim Bouchouchi <karim@netzwiese.com>
2024-11-25 14:13:12 +0100

    Update main.dart
    I updated the default main for the use with Material 3. There are many changes to the Theme Widgets from the flutter Team.

--- starter/bmi_calculator/lib/main.dart
+++ starter/bmi_calculator/lib/main.dart
@@ -6,14 +6,32 @@ class BMICalculator extends StatelessWidget {
   const BMICalculator({super.key});
   @override
   Widget build(BuildContext context) {
+    const appName = "BMI Caluculator";
     return MaterialApp(
-      home: InputPage(),
+      title: appName,
+      theme: ThemeData.dark(
+        // I let this for you to know that we are you using Material 3. It is needed just for Flutter prior 3.16
+        useMaterial3: true,
+        // Theme code for Material 3 was changed. I hope this new template will meet the course requirements but the deafoult appareance is different
+        // This is for a light Theme Z to use it you have to delete the .dark from ThemeData and uncomment
+        // colorScheme: ColorScheme.fromSeed(
+        //        seedColor: Colors.blue, brightness: Brightness.light)
+        //    .copyWith(surfaceTint: Colors.blue),
+        // appBarTheme: AppBarTheme(
+        //     elevation: 4.0, shadowColor: Theme.of(context).colorScheme.shadow),
+        //  textTheme: TextTheme(
+        //    displayLarge:
+        //          TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold)),
+      ),
+      home: InputPage(title: appName),
     );
   }
 }
 
 class InputPage extends StatefulWidget {
-  const InputPage({super.key});
+  final String title;
+  const InputPage({Key? key, required this.title}) : super(key: key);
+
   @override
   _InputPageState createState() => _InputPageState();
 }
@@ -22,16 +40,10 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
-      ),
+      appBar: AppBar(title: Text(widget.title)),
+      body: Center(child: Text('Body Text')),
+      floatingActionButton:
+          FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
     );
   }
 }
