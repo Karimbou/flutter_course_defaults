efcf53a4621d20c35f5fdd05454cad552385f935
Karim Bouchouchi <karim@netzwiese.com>
2024-11-30 09:02:50 +0100

    Update round_icon_button.dart
    Refactored by qwen2.5-coder

--- refactoredByAI/bmi_calculator/lib/components/round_icon_button.dart
+++ refactoredByAI/bmi_calculator/lib/components/round_icon_button.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
 
+// This class represents a custom round icon button widget.
 class RoundIconButton extends StatelessWidget {
+  // The icon to be displayed on the button.
   final IconData icon;
+
+  // The callback function that will be called when the button is pressed.
   final GestureTapCallback? onpressed;
 
+  // Constructor for creating a new instance of this class.
   const RoundIconButton(
       {super.key, required this.icon, required this.onpressed});
 
   @override
   Widget build(BuildContext context) {
+    // Returns a RawMaterialButton widget with the following properties:
+    // - onPressed: The callback function that will be called when the button is pressed.
+    // - elevation: The z-coordinate at which to place the button relative to its parent.
+    // - constraints: A BoxConstraints object specifying the minimum and maximum size of the button.
+    // - shape: A CircleBorder object specifying the shape of the button as a circle.
+    // - fillColor: A Color object specifying the background color of the button.
+    // - child: An Icon widget with the specified icon to be displayed on the button.
     return RawMaterialButton(
       onPressed: onpressed,
       elevation: 1.0,
